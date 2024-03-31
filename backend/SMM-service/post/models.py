151ab4109469fce7c0e6bfe857632890bf0e7318
HghaVlad <vladbax6@gmail.com>
2024-03-31 17:32:13 +0300

    fixes

--- backend/SMM-service/post/models.py
+++ backend/SMM-service/post/models.py
@@ -1,15 +1,19 @@
-from django.db import models
 from django.contrib.postgres import fields
-    
-from workspace.models import Workspace
+from django.db import models
 from user.models import User
+from workspace.models import Workspace
+
 
 class Post(models.Model):
     name = models.CharField(max_length=64)
     text = models.TextField(max_length=4096)
     workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_posts')
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="created_posts"
+    )
     photos = fields.ArrayField(models.FilePathField())
     files = fields.ArrayField(models.FilePathField())
+    send_planned_at = models.DateTimeField()
+
     modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- backend/SMM-service/workspace/models.py
+++ backend/SMM-service/workspace/models.py
@@ -1,11 +1,13 @@
-from django.db import models
-
-from user.models import User
 from channel.models import Channel
+from django.db import models
+from user.models import User
+
 
 class Workspace(models.Model):
     name = models.CharField(max_length=32)
     members = models.ManyToManyField(User)
-    creator_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_workspaces')
+    creator_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="created_workspaces"
+    )
     channels = models.ManyToManyField(Channel)
     created_at = models.DateTimeField(auto_now_add=True)
