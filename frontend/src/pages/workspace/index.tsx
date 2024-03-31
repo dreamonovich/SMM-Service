926ba16ce55e95a4c2a765c26ee74e75559382e6
BebraBebrenko <bebrenkobebra290@gmail.com>
2024-03-31 18:32:17 +0300

    added resizable layout to workspace

--- frontend/src/pages/workspace/index.tsx
+++ frontend/src/pages/workspace/index.tsx
@@ -1,4 +1,10 @@
 import { API_URL } from "@/shared/lib/constants";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "@/shared/ui/resizable";
+import { ScrollArea } from "@/shared/ui/scroll-area";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -14,17 +20,78 @@ export const WorkspacePage = () => {
     setLoading(false);
   };
   useEffect(() => {
-    setLoading(true);
+    setLoading(false);
     getData();
   }, []);
-// добавить красивый лоадер
+  // добавить красивый лоадер
+  const [visible, setVisible] = useState(false);
   return (
     <>
       {loading ? (
         <div>loading</div>
       ) : (
-        <div>
-          Workspace {id} {JSON.stringify(workspace)}
+        <div className="h-[100%]">
+          <ResizablePanelGroup direction="horizontal" className="h-[100%]">
+            <ResizablePanel>
+              <ScrollArea>
+                Workspace {id} {JSON.stringify(workspace)}
+                <button onClick={() => setVisible(!visible)}>test</button>
+                <button
+                  
+                  className={
+                    "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent w-full"}
+                  onClick={() => console.log('bebra')}
+                >
+                  <div className="flex w-full flex-col gap-1">
+                    <div className="flex items-center">
+                      <div className="flex items-center gap-2">
+                        <div className="font-semibold">item.name</div>
+                        {/* {!item.read && (
+                          <span className="flex h-2 w-2 rounded-full bg-blue-600" />
+                        )} */}
+                      </div>
+                      {/* <div
+                        className={cn(
+                          "ml-auto text-xs",
+                          mail.selected === item.id
+                            ? "text-foreground"
+                            : "text-muted-foreground"
+                        )}
+                      >
+                        {formatDistanceToNow(new Date(item.date), {
+                          addSuffix: true,
+                        })}
+                      </div> */}
+                    </div>
+                    <div className="text-xs font-medium">item.subject</div>
+                  </div>
+                  <div className="line-clamp-2 text-xs text-muted-foreground">
+                    item.text.substring(0, 300)
+                  </div>
+                  {/* {item.labels.length ? (
+                    <div className="flex items-center gap-2">
+                      {item.labels.map((label) => (
+                        <Badge
+                          key={label}
+                          variant={getBadgeVariantFromLabel(label)}
+                        >
+                          {label}
+                        </Badge>
+                      ))}
+                    </div>
+                  ) : null} */}
+                </button>
+              </ScrollArea>
+            </ResizablePanel>
+            {visible ? (
+              <>
+                <ResizableHandle />
+                <ResizablePanel>Two</ResizablePanel>
+              </>
+            ) : (
+              <></>
+            )}
+          </ResizablePanelGroup>
         </div>
       )}
     </>
