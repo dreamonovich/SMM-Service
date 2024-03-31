e35b2de1070143827c45e29097bf840c06658422
zethange <maksselles@gmail.com>
2024-03-31 13:35:13 +0300

    fix: ci, layout

--- frontend/src/widgets/layout/index.tsx
+++ frontend/src/widgets/layout/index.tsx
@@ -3,5 +3,5 @@ import { Layout } from "./ui/layout";
 export * from "./ui/layout";
 
 export const generateLayout = (withSidebar: boolean = true) => {
-    return <Layout withSidebar={withSidebar} />
+    return () => <Layout withSidebar={withSidebar} />
 }
