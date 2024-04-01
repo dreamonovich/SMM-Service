61c73906afd57c2a36cc5e8da60c7ce3f1d4b6c1
dreamonovich <dreamonovich@gmail.com>
2024-04-01 22:25:38 +0300

    added posts update view

--- backend/SMM-service/post/serializers.py
+++ backend/SMM-service/post/serializers.py
@@ -1,33 +1,29 @@
 import uuid
+from pathlib import Path
 
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError, NotFound, PermissionDenied
 from rest_framework.fields import SerializerMethodField
 
-from .models import Workspace, Post, PostPhoto, PostFile, PostTemplate
+from user.models import User
+from .models import Workspace, Post, PostPhoto, PostFile
 
 
-class PostPhotoSerializer(serializers.ModelSerializer):
-    url = serializers.FileField(source='photo')
+class CheckPermissionAndGetPostMixin:
+    def check_permissions_and_get_post(self) -> tuple[User, Post]:
+        user = self.context.get("request").user
+        if not (post_id := self.context['view'].kwargs.get('post_id')):
+            raise ValidationError(f"workspace_id is required")
+        if not (post := Post.objects.filter(id=post_id).first()):
+            raise NotFound("post not found")
+        if user not in post.workspace.members.all():
+            raise PermissionDenied("workspace not found")
 
-    class Meta:
-        model = PostPhoto
-        fields = ("url",)
-
-
-class PostFileSerializer(serializers.ModelSerializer):
-    url = serializers.FileField(source='file')
-
-    class Meta:
-        model = PostFile
-        fields = ("url",)
+        return user, post
 
 
-class PostSerializer(serializers.ModelSerializer):
-    files = SerializerMethodField()
-    photos = SerializerMethodField()
-
-    def create(self, validated_data):
+class CheckPermissionAndGetWorkspaceMixin:
+    def check_permissions_and_get_workspace(self) -> tuple[User, Workspace]:
         user = self.context.get("request").user
         if not (workspace_id := self.context['view'].kwargs.get('workspace_id')):
             raise ValidationError(f"workspace_id is required")
@@ -36,17 +32,78 @@ class PostSerializer(serializers.ModelSerializer):
         if user not in workspace.members.all():
             raise PermissionDenied("workspace not found")
 
+        return user, workspace
+
+
+class CreateMediaMixin:
+    def create_media(self, post, media_model, key, raise_exception_if_empty=False):
+        files = self.context['request'].FILES.getlist(key)
+
+        if raise_exception_if_empty and not files:
+            raise ValidationError(f'{key} is empty')
+
+        media_model_objs = []
+        for file in files:
+            file.name = str(uuid.uuid4()) + Path(file.name).suffix
+            media_model_objs.append(media_model(post=post, photo=file))
+        media_objs = media_model.objects.bulk_create(media_model_objs)
+
+        return media_objs
+
+
+class PostPhotoSerializer(serializers.ModelSerializer):
+    url = serializers.FileField(source='photo')
+
+    class Meta:
+        model = PostPhoto
+        fields = ("id", "url",)
+
+
+class PostFileSerializer(serializers.ModelSerializer):
+    url = serializers.FileField(source='file')
+
+    class Meta:
+        model = PostFile
+        fields = ("id", "url",)
+
+
+class PostCreateFileSerializer(CheckPermissionAndGetPostMixin, CreateMediaMixin, serializers.Serializer):
+    url = serializers.FileField(source='file', read_only=True)
+
+    def create(self, validated_data):
+        _, post = self.check_permissions_and_get_post()
+
+        return self.create_media(post, PostFile, "files", raise_exception_if_empty=True)
+
+    class Meta:
+        model = PostFile
+        fields = ("id", "url")
+
+
+class PostCreatePhotoSerializer(CheckPermissionAndGetPostMixin, CreateMediaMixin, serializers.Serializer):
+    url = serializers.FileField(source='photo', read_only=True)
+
+    def create(self, validated_data):
+        _, post = self.check_permissions_and_get_post()
+
+        return self.create_media(post, PostPhoto, "photos", raise_exception_if_empty=True)
+
+    class Meta:
+        model = PostPhoto
+        fields = ("id", "url")
+
+
+class PostSerializer(CheckPermissionAndGetWorkspaceMixin, CreateMediaMixin, serializers.ModelSerializer):
+    files = SerializerMethodField()
+    photos = SerializerMethodField()
+
+    def create(self, validated_data):
+        user, workspace = self.check_permissions_and_get_workspace()
+
         new_post = Post.objects.create(creator=user, workspace=workspace, **validated_data)
 
-        photo_files = self.context['request'].FILES.getlist('photos')
-        for photo_file in photo_files:
-            photo_file.name=str(uuid.uuid4())
-            PostPhoto.objects.create(post=new_post, photo=photo_file)
-
-        file_files = self.context['request'].FILES.getlist('files')
-        for file_file in file_files:
-            file_file.name = str(uuid.uuid4())
-            PostFile.objects.create(post=new_post, file=file_file)
+        self.create_media(new_post, PostPhoto, 'photos')
+        self.create_media(new_post, PostFile, 'files')
 
         return new_post
 
@@ -58,25 +115,8 @@ class PostSerializer(serializers.ModelSerializer):
         files = PostFile.objects.filter(post=obj)
         return PostFileSerializer(files, many=True).data
 
+
     class Meta:
         model = Post
         read_only_fields = ("id", "creator", "workspace", "modified_at", "created_at", "photos", "files")
-        fields = read_only_fields + ("name", "text", "send_planned_at", "number_of_confirmations")
-
-
-class PostTemplateSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        user = self.context.get("request").user
-        if not (workspace_id := self.context['view'].kwargs.get('workspace_id')):
-            raise ValidationError(f"workspace_id is required")
-        if not (workspace := Workspace.objects.filter(id=workspace_id).first()):
-            raise NotFound("workspace not found")
-        if user not in workspace.members.all():
-            raise PermissionDenied("workspace not found")
-
-        return PostTemplate.objects.create(creator=user, workspace=workspace, **validated_data)
-
-    class Meta:
-        model = Post
-        fields = ("id", "text", "workspace", "modified_at", "created_at", "name")
-        read_only_fields = ("id", "workspace", "modified_at", "created_at")
+        fields = read_only_fields + ("name", "text", "send_planned_at", "number_of_people")
--- backend/SMM-service/post/views.py
+++ backend/SMM-service/post/views.py
@@ -1,13 +1,15 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Post, PostTemplate
-from .serializers import PostSerializer, PostTemplateSerializer
+from rest_framework.response import Response
+
+from .models import Post, PostFile, PostPhoto
+from .serializers import PostSerializer, PostCreateFileSerializer, PostCreatePhotoSerializer
 
 
 class WorkspacePostListCreateView(ListCreateAPIView):
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         if not (workspace_id := self.kwargs.get('workspace_id')):
@@ -16,28 +18,44 @@ class WorkspacePostListCreateView(ListCreateAPIView):
         return Post.objects.filter(workspace_id=workspace_id, workspace__members__in=(self.request.user,))
 
 
-class PostRetrieveDeleteView(RetrieveDestroyAPIView):
+class PostRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Post.objects.filter(workspace__members__in=(self.request.user,))
 
 
-class WorkspacePostTemplateListCreateView(ListCreateAPIView):
-    serializer_class = PostTemplateSerializer
-    permission_classes = (IsAuthenticated,)
+class PostMediaCreateView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if not (media_type := self.request.data.get('type')):
+            raise ValidationError("type required")
+
+        if media_type == "files":
+            return PostCreateFileSerializer
+        elif media_type == "photos":
+            return PostCreatePhotoSerializer
+
+        raise ValidationError("type invalid")
+
+
+class PostMediaDeleteView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        if not (workspace_id := self.kwargs.get('workspace_id')):
-            raise ValidationError("workspace_id is required")
+        if not (media_type := self.request.data.get('type')):
+            raise ValidationError('type required')
 
-        return PostTemplate.objects.filter(workspace_id=workspace_id, workspace__members__in=(self.request.user,))
+        if media_type == "files":
+            return PostFile.objects.filter(post__workspace__members__in=(self.request.user,))
+        elif media_type == "photos":
+            return PostPhoto.objects.filter(post__workspace__members__in=(self.request.user,))
 
+        raise ValidationError('type not valid')
 
-class PostTemplateRetrieveDeleteView(RetrieveDestroyAPIView):
-    serializer_class = PostTemplateSerializer
-    permission_classes = (IsAuthenticated,)
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
 
-    def get_queryset(self):
-        return Post.objects.filter(workspace__members__in=(self.request.user,))
+        return Response({})
