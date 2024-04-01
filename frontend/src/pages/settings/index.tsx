768f22f8d3b751ac2ae9dd43d711549a994fede7
zethange <maksselles@gmail.com>
2024-04-01 17:46:21 +0300

    fix: remove unused imports

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -1,21 +1,9 @@
 import { AddMember } from "@/features/workspace/add-member";
-import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
-import { Button } from "@/shared/ui/button";
-import { Input } from "@/shared/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/shared/ui/select";
 
 export function Settings() {
-  //get from backend
-  const link = "https://bebra.com";
   return (
     <>
-    <AddMember />
+      <AddMember />
     </>
   );
 }
