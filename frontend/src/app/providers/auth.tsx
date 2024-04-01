27f25e9880f6828380c3bd79679148a90f2044f4
zethange <maksselles@gmail.com>
2024-04-01 11:18:12 +0300

    add authorization

--- frontend/src/app/providers/auth.tsx
+++ frontend/src/app/providers/auth.tsx
@@ -2,10 +2,11 @@ import { useUserStore } from "@/entities/user";
 import { API_URL } from "@/shared/lib/constants";
 import { User } from "@/shared/lib/types";
 import { FC, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export const AuthProvider: FC = () => {
   const user = useUserStore();
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
       const res = await fetch(API_URL + "/me", {
@@ -14,8 +15,7 @@ export const AuthProvider: FC = () => {
         }
       });
       if (!res.ok) {
-        console.log("not registered")
-        //navigate("/login");
+        navigate("/login");
       } else {
         user.setUser((await res.json()) as User);
       }
