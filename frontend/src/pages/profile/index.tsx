bb0cdd4dc32654a6f249307a1775f5edf4c391ba
zethange <maksselles@gmail.com>
2024-04-01 18:59:14 +0300

    fix: login

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -69,7 +69,7 @@ export const Profile = () => {
             )}
           />
           <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="picture">Picture</Label>
+            <Label>Picture</Label>
             <Input id="picture" type="file" />
           </div>
           <Button type="submit">Submit</Button>
