df5e46c6160e5b08d554902e5c4f8fc88e44947e
dreamonovich <dreamonovich@gmail.com>
2024-04-02 11:39:20 +0300

    deleted analytics

--- backend/SMM-service/analytics/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from analytics.views import WorkspaceChannels
-
-urlpatterns = [
-    path('workspace/<int:workspace_id>/channels/', WorkspaceChannels.as_view(), name='workspace_channels'),
-]
