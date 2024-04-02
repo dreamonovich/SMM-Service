c94af78cc8831bd88010878027846a0b60525aa1
dreamonovich <dreamonovich@gmail.com>
2024-04-02 18:16:05 +0300

    files test (DANGER) 2

--- backend/SMM-service/telegram/utils.py
+++ backend/SMM-service/telegram/utils.py
@@ -9,7 +9,7 @@ bot = TeleBot(TOKEN)
 
 def send_post(channel_id, text, photos=[], files=[]):
     photo_group = [InputMediaPhoto(photo.photo, caption=text) for photo in photos]
-    logging.info([photo.photo for photo in photos])
+    logging.info(f"{[photo.photo for photo in photos]}")
     if photos:
         bot.send_media_group(channel_id, photo_group)
     else:
