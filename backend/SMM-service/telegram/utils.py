aa440950227ab1c1784106c8473d248e01a4d4b0
dreamonovich <dreamonovich@gmail.com>
2024-04-02 19:29:59 +0300

    files fix (DANGER) 8

--- backend/SMM-service/telegram/utils.py
+++ backend/SMM-service/telegram/utils.py
@@ -9,13 +9,13 @@ bot = TeleBot(TOKEN)
 
 def send_post(channel_id, text, photos=[], files=[]):
     photo_group = [InputMediaPhoto(photo, caption=text) for photo in photos]
+    bot.send_message(channel_id, text=f"{photos}")
     if photos:
         bot.send_media_group(channel_id, photo_group)
     else:
         bot.send_message(channel_id, text, parse_mode="Markdown")
     for file in files:
         bot.send_document(channel_id, InputFile(file.file))
-    bot.send_message(channel_id, text=f"{photos}")
 
 
 def delete_message(chat_id, message_id):
