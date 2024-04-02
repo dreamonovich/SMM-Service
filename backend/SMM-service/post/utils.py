0ab648ffe6cadd933ed858ce2a3e53ca9542cdd8
dreamonovich <dreamonovich@gmail.com>
2024-04-02 20:10:58 +0300

    fixed files

--- backend/SMM-service/post/utils.py
+++ backend/SMM-service/post/utils.py
@@ -1,5 +1,6 @@
 from telebot import TeleBot
 from telebot.types import InputMediaPhoto, InputFile, InlineKeyboardMarkup, InlineKeyboardButton
+from io import BytesIO
 
 TOKEN = "6755435757:AAEdJcrtEuEmYz2feDl0I0bG5fbf5MpFGoA"
 
@@ -10,11 +11,11 @@ def send_message(chat_id, text, post_id, photos=[], files=[]):
     bot.send_message(chat_id, text=f"{files}", parse_mode="Markdown")
     if photos:
         photo_group = [InputMediaPhoto(photo.photo.file.read(), caption=text) for photo in photos]
-        bot.send_media_group(chat_id, photo_group)
+        # bot.send_media_group(chat_id, photo_group)
     else:
         bot.send_message(chat_id, text, parse_mode="Markdown")
     for file in files:
-        bot.send_document(chat_id, InputFile(file.file.file.read()))
+        bot.send_document(chat_id, InputFile(BytesIO(file.file.file.read())))
 
     bot.send_message(chat_id, "*Вы принимаете этот пост?*", parse_mode="Markdown", reply_markup=get_keyboard(post_id))
 
