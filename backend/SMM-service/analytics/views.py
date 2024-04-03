21239ad0070c19b67f62b05b897ae5f3d7230684
dreamonovich <dreamonovich@gmail.com>
2024-04-03 04:26:21 +0300

    clean except

--- backend/SMM-service/analytics/views.py
+++ backend/SMM-service/analytics/views.py
@@ -24,28 +24,26 @@ class WorkspaceChannels(APIView):
         ]
     )
     def get(self, request, workspace_id):
-        try:
-            channels = Channel.objects.filter(workspace_id=workspace_id)
-            workspace_data = {}
-            for channel in channels:
-                telegram_posts = TelegramPost.objects.filter(telegram_channel=channel).all()
-                telegram_post_serializer = TelegramPostSerializer(telegram_posts, many=True)
 
-                posts_with_channel_username = []
-                for post_data in telegram_post_serializer.data:
-                    if not channel.is_group:
-                        post_data['channel_name'] = channel.channel_username
-                        posts_with_channel_username.append(post_data)
+        channels = Channel.objects.filter(workspace_id=workspace_id)
+        workspace_data = {}
+        for channel in channels:
+            telegram_posts = TelegramPost.objects.filter(telegram_channel=channel).all()
+            telegram_post_serializer = TelegramPostSerializer(telegram_posts, many=True)
 
-                workspace_data[channel.name] = posts_with_channel_username
+            posts_with_channel_username = []
+            for post_data in telegram_post_serializer.data:
+                if not channel.is_group:
+                    post_data['channel_name'] = channel.channel_username
+                    posts_with_channel_username.append(post_data)
+
+            workspace_data[channel.name] = posts_with_channel_username
 
 
-            updated_workspace_data = update_workspace_data(workspace_data)
-            updated_workspace_data = update_workspace_data_list(updated_workspace_data)
-            if updated_workspace_data:
-                return Response({"workspace_data": updated_workspace_data}, status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "Failed to update workspace data"},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        updated_workspace_data = update_workspace_data(workspace_data)
+        updated_workspace_data = update_workspace_data_list(updated_workspace_data)
+        if updated_workspace_data:
+            return Response({"workspace_data": updated_workspace_data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Failed to update workspace data"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
